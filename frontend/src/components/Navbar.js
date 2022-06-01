f2cb994f4a99c851424073b0183e7383a28b98e1
EzeMartino <ezequielrmartino@gmail.com>
2022-06-01 20:37:26 -0300

    Borre boton de bootstrap para ver login y register

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,17 +7,6 @@ const Navbar = () => {
         <a class="navbar-brand" href="#">
           Navbar
         </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
         <form class="d-flex">
           <input
             class="form-control me-2"
