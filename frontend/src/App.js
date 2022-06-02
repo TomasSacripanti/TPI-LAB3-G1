df528ebc13ee3c5dcb90a754a7c8017ba480261f
EzeMartino <ezequielrmartino@gmail.com>
2022-06-01 21:14:50 -0300

    Dummy y div con animes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,37 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Animes from "./components/Animes";
+
+const DUMMY_ANIMES = [
+  {
+    id: 1,
+    title: "Sword Art Online",
+    chaptersCount: 24,
+    comment: "Empieza bien, pero después del capítulo 10, decae mucho",
+  },
+  {
+    id: 2,
+    title: "Black Clover",
+    chaptersCount: 170,
+    comment: "Shonen de peleas que está piola pero tarda en arrancar",
+  },
+  {
+    id: 3,
+    title: "SpyXFamily",
+    chaptersCount: 12,
+    comment: "Slice of life muy bueno y divertido",
+  },
+];
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <Animes animeList={DUMMY_ANIMES} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
