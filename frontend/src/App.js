fa41250635a35739e479572ab7a606e1c7442e18
EzeMartino <ezequielrmartino@gmail.com>
2022-06-01 21:43:48 -0300

    Dummy peliculas y series al frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 import Animes from "./components/Animes";
+import Series from "./components/Series";
 
 const DUMMY_ANIMES = [
   {
@@ -23,12 +25,59 @@ const DUMMY_ANIMES = [
   },
 ];
 
+const DUMMY_SERIES = [
+  {
+    id: 1,
+    title: "Peaky Blinders",
+    chaptersCount: 36,
+    comment: "Muy picante de principio a fin",
+  },
+  {
+    id: 2,
+    title: "Vikings",
+    chaptersCount: 89,
+    comment: "Aguante Ragnar lcdsm, a pedazos a partir de la temporada 4",
+  },
+  {
+    id: 3,
+    title: "Breaking Bad",
+    chaptersCount: 62,
+    comment: "VAMOS A HACER META",
+  },
+];
+
+const DUMMY_MOVIES = [
+  {
+    id: 1,
+    title: "El Show de Truman",
+    director:"Peter Weir",
+    duration: 103,
+    comment: "Tremendo plot twist",
+  },
+  {
+    id: 2,
+    title: "Gangster Americano",
+    director:"Ridley Scott",
+    duration: 176,
+    comment: "Peliculón de chill",
+  },
+  {
+    id: 3,
+    title: "El Señor de los Anillos: El Retorno del Rey",
+    director:"Peter Jackson",
+    duration: 201,
+    comment: "La única tercera pelicula mejor que las anteriores",
+  },
+];
 function App() {
   return (
     <div>
       <Navbar />
       <div>
+        <h3>Lista de animes </h3>
         <Animes animeList={DUMMY_ANIMES} />
+        <h3>Lista de series </h3>
+        <Series seriesList={DUMMY_SERIES} />
       </div>
     </div>
   );
