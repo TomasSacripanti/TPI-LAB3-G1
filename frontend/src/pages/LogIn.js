7b01c1e125c75d4bc7a67cbbe61994ae7d398b3a
JL <josemedina1610@gmail.com>
2022-06-15 22:12:24 -0300

    Add LogIn and Register Forms

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,22 +1,10 @@
 import React from 'react'
+import LogInForm from '../components/forms/LogInForm'
 
 const LogIn = () => {
   return (
     <div>
-        <h3>Log In</h3>
-        <p>Usuario</p>
-        <input
-        type='text'
-        placeholder='Ingrese su usuario'/>
-        <p>Contraseña</p>
-        <input
-        type='password'
-        placeholder='Ingrese su contraseña'/>
-        <br></br>
-
-        <button>Ingresar</button>
-        <button>Registrarse</button>
-     
+        <LogInForm/>
     </div>
   )
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import RegisterForm from '../components/forms/RegisterForm'
 
 const Register = () => {
   return (
     <div>
-        <></>
+        <RegisterForm/>
     </div>
   )
 }
