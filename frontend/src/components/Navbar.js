b530bff6be0eb4bdca60c35c9a23f295e1fe4dae
Tomás Sacripanti <sacripantitomas@gmail.com>
2022-06-22 21:43:03 -0300

    Cambiamos textos en navbar de reviews a contents

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = ({ isLogged }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-        <div className="navbar-brand">Reviews App</div>
+        <div className="navbar-brand">App de contenidos</div>
         <div className="" id="navbarNav">
           <ul className="navbar-nav">
-            {isLogged ? <li className="nav-item"><Link className="nav-link" to="/contents/add">Agregar Review</Link></li> : <li className="nav-item"><Link className="nav-link" to="/login">Ingresar</Link></li>}
-            {isLogged ? <li className="nav-item"><Link className="nav-link" to="/contents">Reviews</Link></li> : <li className="nav-item"><Link className="nav-link" aria-current="page" to="/register">Registrarse</Link></li>}
+            {isLogged ? <li className="nav-item"><Link className="nav-link" to="/contents/add">Agregar contenido</Link></li> : <li className="nav-item"><Link className="nav-link" to="/login">Ingresar</Link></li>}
+            {isLogged ? <li className="nav-item"><Link className="nav-link" to="/contents">Contenidos</Link></li> : <li className="nav-item"><Link className="nav-link" aria-current="page" to="/register">Registrarse</Link></li>}
           </ul>
         </div>
       </div>
