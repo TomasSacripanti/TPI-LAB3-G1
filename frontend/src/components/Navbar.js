e4a646be21a5bc6c0f1ada0e7aba762e7df3039e
EzeMartino <ezequielrmartino@gmail.com>
2022-06-22 22:10:56 -0300

    Boton

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchForm from "./forms/SearchForm";
+import ChangeTheme from "./ChangeTheme";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
@@ -13,6 +14,7 @@ const Navbar = ({ isLogged }) => {
           <ul className="navbar-nav">
             {isLogged ? <li className="nav-item"><Link className="nav-link" to="/add">Agregar Review</Link></li> : <li className="nav-item"><Link className="nav-link" to="/login">Ingresar</Link></li>}
             {isLogged ? <li className="nav-item"><Link className="nav-link" to="/reviews">Reviews</Link></li> : <li className="nav-item"><Link className="nav-link" aria-current="page" to="/register">Registrarse</Link></li>}
+            <li className="nav-link"><ChangeTheme/></li>
           </ul>
         </div>
       </div>
