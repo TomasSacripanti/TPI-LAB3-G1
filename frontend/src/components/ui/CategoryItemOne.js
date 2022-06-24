5b5def09d4e37dfe84df5c972317d12fd2a13093
Tomás Sacripanti <sacripantitomas@gmail.com>
2022-06-24 20:18:59 -0300

    Modo nocturno aplicado a seccion de contenidos

--- frontend/src/components/ui/CategoryItemOne.js
+++ frontend/src/components/ui/CategoryItemOne.js
@@ -1,7 +1,28 @@
+import {useEffect, useContext} from "react";
+import ThemeContext from "../../context/ThemeContext";
 import {Card} from "react-bootstrap";
 import "./CategoryItem.css";
 
 const CategoryItemOne = ({ title, chaptersCount, comment }) => {
+  const {theme} = useContext(ThemeContext); 
+
+  useEffect(() => {
+    const cards = document.getElementsByClassName("card");
+    for (let i = 0; i < cards.length; i++) {
+      if (theme === "dark") {
+        cards[i].classList.remove("bg-light");
+        cards[i].classList.add("bg-dark");
+        cards[i].classList.remove("card-light");
+        cards[i].classList.add("card-dark");
+      } else {
+        cards[i].classList.remove("bg-dark");
+        cards[i].classList.add("bg-light");
+        cards[i].classList.remove("card-dark");
+        cards[i].classList.add("card-light");
+      }
+    }
+  }, [theme])
+
   return (
     <Card className="item-container">
       <Card.Img variant="top"/>
--- frontend/src/pages/Contents.js
+++ frontend/src/pages/Contents.js
@@ -1,125 +1,36 @@
-// import {useState} from "react";
-import SearchForm from "../components/forms/SearchForm";
+import { useEffect, useContext} from "react";
 import CategorySection from "../components/CategorySection";
-
-const DUMMY_ANIMES = [
-  {
-    id: 1,
-    title: "Sword Art Online",
-    chaptersCount: 24,
-    comment: "Empieza bien, pero después del capítulo 10, decae mucho",
-  },
-  {
-    id: 2,
-    title: "Black Clover",
-    chaptersCount: 170,
-    comment: "Shonen de peleas que está piola pero tarda en arrancar",
-  },
-  {
-    id: 3,
-    title: "SpyXFamily",
-    chaptersCount: 12,
-    comment: "Slice of life muy bueno y divertido",
-  },
-  {
-    id: 4,
-    title: "Sword Art Online",
-    chaptersCount: 24,
-    comment: "Empieza bien, pero después del capítulo 10, decae mucho",
-  },
-  {
-    id: 5,
-    title: "Black Clover",
-    chaptersCount: 170,
-    comment: "Shonen de peleas que está piola pero tarda en arrancar",
-  },
-  {
-    id: 6,
-    title: "SpyXFamily",
-    chaptersCount: 12,
-    comment: "Slice of life muy bueno y divertido",
-  },
-];
-
-const DUMMY_SERIES = [
-  {
-    id: 7,
-    title: "Peaky Blinders",
-    chaptersCount: 36,
-    comment: "Muy picante de principio a fin",
-  },
-  {
-    id: 8,
-    title: "Vikings",
-    chaptersCount: 89,
-    comment: "Aguante Ragnar lcdsm, a pedazos a partir de la temporada 4",
-  },
-  {
-    id: 9,
-    title: "Breaking Bad",
-    chaptersCount: 62,
-    comment: "VAMOS A HACER META",
-  },
-];
-
-const DUMMY_MOVIES = [
-  {
-    id: 10,
-    title: "El Show de Truman",
-    director: "Peter Weir",
-    duration: 103,
-    comment: "Tremendo plot twist",
-  },
-  {
-    id: 11,
-    title: "Gangster Americano",
-    director: "Ridley Scott",
-    duration: 176,
-    comment: "Peliculón de chill",
-  },
-  {
-    id: 12,
-    title: "El Señor de los Anillos: El Retorno del Rey",
-    director: "Peter Jackson",
-    duration: 201,
-    comment: "La única tercera pelicula mejor que las anteriores",
-  },
-];
-
-const DUMMY_MANGAS = [
-  {
-    id: 13,
-    title: "Berserk",
-    chaptersCount: 364,
-    comment: "Obra maestra, de los mejores mangas que existen.",
-  },
-  {
-    id: 14,
-    title: "Magi",
-    chaptersCount: 369,
-    comment:
-      "Shonen de aventura, muy creativo pero con un final controversial.",
-  },
-  {
-    id: 15,
-    title: "Dragon Ball",
-    chaptersCount: 520,
-    comment: "Clasico de clasicos, goku le gana.",
-  },
-];
+import ThemeContext from "../context/ThemeContext";
+import DATA from "../Data";
+import "./Contents.css";
 
 const Contents = () => {
+  const {theme} = useContext(ThemeContext); 
+
+  useEffect(() => {
+    const categoryTitle = document.getElementsByClassName("category-title");
+    for (let i = 0; i < categoryTitle.length; i++) {
+      if (theme === "dark") {
+        categoryTitle[i].classList.remove("category-title-light");
+        categoryTitle[i].classList.add("category-title-dark");
+        console.log(categoryTitle[i].classList);
+      } else {
+        categoryTitle[i].classList.remove("category-title-dark");
+        categoryTitle[i].classList.add("category-title-light");
+        console.log(categoryTitle[i].classList);
+      }
+    }
+  }, [theme])
   return (
     <>
-      <SearchForm></SearchForm>
-      <h3>Lista de animes </h3>
-      <CategorySection list={DUMMY_ANIMES} category="animes" />
-      <h3>Lista de mangas </h3>
-      <CategorySection list={DUMMY_MANGAS} category="mangas" />
-      <h3>Lista de series </h3>
-      <CategorySection list={DUMMY_SERIES} category="series" />
-      <h3>Lista de películas</h3>
-      <CategorySection list={DUMMY_MOVIES} category="peliculas" />
+      <h3 className="category-title">Lista de animes </h3>
+      <CategorySection list={DATA.DUMMY_ANIMES} category="animes" />
+      <h3 className="category-title">Lista de mangas </h3>
+      <CategorySection list={DATA.DUMMY_MANGAS} category="mangas" />
+      <h3 className="category-title">Lista de series </h3>
+      <CategorySection list={DATA.DUMMY_SERIES} category="series" />
+      <h3 className="category-title">Lista de películas</h3>
+      <CategorySection list={DATA.DUMMY_MOVIES} category="peliculas" />
     </>
   );
 };
