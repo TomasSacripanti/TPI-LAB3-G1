538d7cdb23235c00c411dc248e87adc33bcfe269
JL <josemedina1610@gmail.com>
2022-07-02 01:36:20 -0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,33 +10,36 @@ import Login from "./pages/Login";
 import { ThemeContextProvider } from "./context/ThemeContext";
 import { LoginContextProvider } from "./context/LoginContext";
 import { DataContextProvider } from "./context/DataContext";
+import { UsersListContextProvider } from "./context/UsersListContext";
 
 const App = () => {
   return (
     <>
       <ThemeContextProvider>
         <LoginContextProvider>
-          <DataContextProvider>
-            <Navbar />
-            <div className="container">
-              <Routes>
-                <Route
-                  path="/"
-                  element={<Navigate replace to="/login" />}
-                ></Route>
+          <UsersListContextProvider>
+            <DataContextProvider>
+              <Navbar />
+              <div className="container">
+                <Routes>
+                  <Route
+                    path="/"
+                    element={<Navigate replace to="/login" />}
+                  ></Route>
 
-                <Route path="/contents" element={<Contents />}></Route>
-                <Route path="/contents/add" element={<AddContent />}></Route>
+                  <Route path="/contents" element={<Contents />}></Route>
+                  <Route path="/contents/add" element={<AddContent />}></Route>
 
-                <Route
-                  path="/contents/popular"
-                  element={<PopularContent />}
-                ></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/register" element={<Register />}></Route>
-              </Routes>
-            </div>
-          </DataContextProvider>
+                  <Route
+                    path="/contents/popular"
+                    element={<PopularContent />}
+                  ></Route>
+                  <Route path="/login" element={<Login />}></Route>
+                  <Route path="/register" element={<Register />}></Route>
+                </Routes>
+              </div>
+            </DataContextProvider>
+          </UsersListContextProvider>
         </LoginContextProvider>
       </ThemeContextProvider>
     </>
